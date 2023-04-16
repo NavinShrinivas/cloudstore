23421d8c63a2820b80e93a7691e5c74439590c35
Mohamed Ayaan <ayaan35813@gmail.com>
2023-04-16 17:48:40 +0530

    Added order page

--- cloudstore_site/src/pages/cart.page.js
+++ cloudstore_site/src/pages/cart.page.js
@@ -3,24 +3,27 @@ import { HomeNavbar } from '../components'
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux'
 import { login, logout, cart } from '../redux/features/userSlice'
-
 import axios from "axios";
 import { useEffect } from 'react';
 
 function CartPage() {
-    var cart = useSelector((state) => state.user.cart)
-    console.log(cart)
-    useEffect(() => {
+    const cart = useSelector((state) => state.user.cart)
+    const newArr = [...cart];
 
-    }, [])
+    const senddetails = () => {
+
+    }
     return (
         <>
             <HomeNavbar />
             <div style={{ marginTop: "100px" }}></div>
-            {cart.map(prod => {
+            {newArr.map(prod => {
+                // const deleteelement = (id) => {
+                //     newArr.splice(id, 1)
+                // }
 
                 return (
-                    <Container>
+                    <Container key={prod.ID}>
                         <Row>
                             <Col>ID:{prod.ID}</Col>
                             <Col>{prod.name}</Col>
@@ -29,13 +32,16 @@ function CartPage() {
                             <Col>{prod.seller_username}</Col>
                             <Col>{prod.price} $</Col>
                         </Row>
-
+                        {/* <Button onClick={() => { deleteelement(2) }}>X</Button> */}
                         <hr />
                     </Container>
                 )
             }
 
             )}
+            <center>
+                <Button onClick={() => { }}>Make order</Button>
+            </center>
         </>
     );
 }
