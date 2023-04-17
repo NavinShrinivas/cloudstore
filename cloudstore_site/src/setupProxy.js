b1eda3a43b2fdc0d59c28433a4f31d3cc8c231b5
NavinShrinivas <karupal2002@gmail.com>
2023-04-17 19:06:03 +0530

    origins fix for deployment

--- cloudstore_site/src/setupProxy.js
+++ cloudstore_site/src/setupProxy.js
@@ -1,31 +1,31 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
    app.use(
       '/api/account',
       createProxyMiddleware({
-         target: 'http://192.168.49.2:80',
+         target: 'http://hackframe.navinxyz.com:8081',
          changeOrigin: true,
       })
    );
    app.use(
       '/api/products',
       createProxyMiddleware({
-         target: 'http://192.168.49.2:80',
+         target: 'http://hackframe.navinxyz.com:8081',
          changeOrigin: true,
       })
    );
    app.use(
       '/api/orders',
       createProxyMiddleware({
-         target: 'http://192.168.49.2:80',
+         target: 'http://hackframe.navinxyz.com:8081',
          changeOrigin: true,
       })
    );
    // app.use(
    //     '/api/reviews',
    //     createProxyMiddleware({
-   //         target: 'http://192.168.49.2:80',
+   //         target: 'http://hackframe.navinxyz.com:8081',
    //         changeOrigin: true,
    //     })
    // );
