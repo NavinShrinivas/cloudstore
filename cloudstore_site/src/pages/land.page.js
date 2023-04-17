2e1bc4583f1bac6f6ef60c8110b5da8c32a47ec4
Mitul Joby <73733877+Mitul-Joby@users.noreply.github.com>
2023-04-17 23:54:48 +0530

    Update land.page.js

--- cloudstore_site/src/pages/land.page.js
+++ cloudstore_site/src/pages/land.page.js
@@ -13,7 +13,7 @@ function LandPage() {
                         <div className="d-flex flex-column align-items-center" style={{ width: '80%' }}>
                             <img src={CloudstoreLogo} alt="Cloudstore" style={{ width: '100%' }} />
                             <div className="text-white text-center my-3" style={{ fontSize: '1.5rem' }}>
-                                Cloudstore is a ...
+                                Cloudstore is a scalable microservice deployed web store
                             </div>
                         </div>
                         <div className="d-flex justify-content-around">
